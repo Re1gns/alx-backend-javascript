bb3d5a629b08bc6e115790cda683ece9bc1f2816
Re1gns <lreigns12@gmail.com>
2023-11-24 12:30:04 +0100

    Fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,14 +3,10 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const input = data.toString().trim();
 
-  if (input === 'exit') {
+  if (input.toLowerCase() === 'exit') {
     process.stdout.write('This important software is now closing\n');
     process.exit();
-  } else {
-    process.stdout.write(`Your name is: ${input}\n`);
   }
-});
 
-process.on('exit', () => {
-  process.stdout.write('This important software is now closing\n');
+  process.stdout.write(`Your name is: ${input}\n`);
 });
