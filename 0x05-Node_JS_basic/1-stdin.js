a3d4d2b14654484545f37a90e299e70b43092f5b
Re1gns <lreigns12@gmail.com>
2023-11-24 12:33:47 +0100

    Fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -10,3 +10,8 @@ process.stdin.on('data', (data) => {
 
   process.stdout.write(`Your name is: ${input}\n`);
 });
+
+process.stdin.once('end', () => {
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
+});
